b90caf128a95860ad39d5d6ee8d56b91052fbede
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-07-23 22:28:40 -0500

    Clase de variables

---
+++ clase-variables.js
@@ -0,0 +1,12 @@
+let cajaAndy = 'Woody'
+console.log(cajaAndy)
+
+// Lo NO permitido
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+// Lo permitido
+let primerTrasteoDeAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDeUsuario = '1234567890'
