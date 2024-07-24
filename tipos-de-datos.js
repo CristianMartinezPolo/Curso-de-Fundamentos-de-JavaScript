034c536960c6c8bac7a27e1f5198cbdcf163517b
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-07-23 22:37:08 -0500

    Tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,38 @@
+// PRIMITIVOS
+
+//String
+let nombre = 'Cristian'
+
+//number
+let edad = 25
+
+//boolean
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol('único')
+
+//bigint
+let numeroGrande = 2n
+
+//COMPLEJOS
+
+//object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//arrays
+let frutas = ['manzana', 'banao', 'uvas']
+
+//function
+function saludar () {
+    
+}
